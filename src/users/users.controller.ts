947f6eef9f89291acc027e5efbfbe093bec54f95
maleeshaiqzsystems <maleesha.m@iqzsystems.com>
2024-10-14 11:45:27 +0530

    dependency injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { strict } from 'assert';
+import { UsersService } from './users.service';
 
 @Controller('users') //decorators
-export class UsersController {}
+export class UsersController {
+
+    // add instance of userServivce (injecting)
+    constructor(private readonly userService : UsersService) {
+        
+    }
+    //creating routes
+
+    // @Get() //GET /users or /users?role=value    (role=value   this is the query parameter)
+    // findAll(){
+    //     return []
+    // }
+
+    @Get() //GET /users or /users?role=value    (role=value   this is the query parameter)
+    findAll(@Query('role') role?: 'Intern'|'SEO' | 'ASE'| 'Admin'){
+        return this.userService.findAll(role)
+    }
+
+    @Get('interns') //GET /users/interns
+    findAllInters(){
+        return this.userService.findAll('Intern')
+    }
+
+    //this route read anything afer users/ as a id
+    @Get(':id') //GET /users/:id 
+    findOne(@Param('id') id:number){
+        return this.userService.findOne(id);
+    }
+
+    @Post() //POST /users
+    create(@Body() user: {}){
+        return user
+    } 
+
+    @Patch(':id') //PATCH /users/:id 
+    update(@Param('id') id:string, @Body() userUpdate:{}){
+        return {id,...userUpdate }
+    }
+
+    @Delete(':id') //GET /users/:id 
+    delete(@Param('id') id:string){
+        return {id}
+    }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,80 @@
 import { Injectable } from '@nestjs/common';
+import { error } from 'console';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id" : 1,
+            "name": "Maleesha",
+            "email": "mal@gmail.com",
+            "role": "ASE"
+        },
+        {
+            "id" : 2,
+            "name": "Thisura",
+            "email": "tis@gmail.com",
+            "role": "SEO"
+        },
+        {
+            "id" : 3,
+            "name": "Dilki",
+            "email": "dil@gmail.com",
+            "role": "Admin"
+        },
+        {
+            "id" : 4,
+            "name": "Nesandi",
+            "email": "nes@gmail.com",
+            "role": "Intern"
+        },
+        {
+            "id" : 5,
+            "name": "Dilmi",
+            "email": "dil@gmail.com",
+            "role": "Intern"
+        }
+    ]
+
+    //creat the functions which are in the controller file.(eg:findOne(), findAll(), update(), delete())
+
+    findAll(role?:'Intern'|'SEO' | 'ASE'| 'Admin'){
+        if(role){
+            return this.users.filter(users => users.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id: number){
+        const user = this.users.find(user => user.id === id)
+        return user
+    }
+
+    create(user: { name: string, email: string, role :'Intern'|'SEO' | 'ASE'| 'Admin' }){
+        const usersByHighestId = [...this.users].sort((a,b)=>b.id-a.id)
+        const newUser = {
+            id: usersByHighestId[0].id+1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser 
+    }
+
+    update(id: number, updatedUser : { name?: string, email?: string, role ?:'Intern'|'SEO' | 'ASE'| 'Admin' }){
+        this.users = this.users.map(user =>{
+            if(user.id ===id){
+                return {...user, ...updatedUser}
+            }
+            return user
+        })
+        return this.findOne(id)
+    }
+
+    delete(id:number){
+        const removedUser = this.findOne(id)
+
+        this.users= this.users.filter(user => user.id !== id)
+
+        return removedUser
+    }
+}
